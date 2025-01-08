abc108154ad69ac9fe718711e0c939e72fd36eae
tomasnieto02 <tomas.andres.nieto@gmail.com>
2025-01-07 18:45:19 -0600

    Paddle Movement
    Added paddle movement.
    Press A to move left and press D to move right.

---
+++ Assets/Paddle.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.name = "Paddle";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool left = Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.D);
+        if (left && transform.position.x > -8)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        else if (right && transform.position.x < 8)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime *speed);
+        } 
+    }
+}
---
+++ Assets/Quit.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+   // Quit application
+   public void quitGame()
+   {
+        Application.Quit();
+   }
+}
