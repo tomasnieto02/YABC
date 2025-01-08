150ddee0b3cb95cf45992c863bcee6ec0d726015
tomasnieto02 <tomas.andres.nieto@gmail.com>
2025-01-07 23:35:54 -0600

    Ball bounce
    Implemented ball bounce on paddle and walls.

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -4,22 +4,62 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
+    // Bool variable for knowing if there is a ball currently in play.
     public bool playBall = false;
+    // Variable for setting an offset to the paddle when the ball isn't in play.
     public float ballOnPadOffset;
+    // Variable for controlling the ball's speed.
+    public float ballSpeed;
+    // Variable for calculating the ball's direction when it bounces on the paddle.
+    public float newDirFactor;
+    // Vector for moving the ball in a direction.
+    Vector2 direction;
+    // Reference for accesing paddleScript's variables.
     [SerializeField] private Paddle paddleScript;
+    // Ball's rigid body.
     public Rigidbody2D ballRb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created.
     void Start()
     {
-        
+        gameObject.name = "Ball";
+        ballRb = GetComponent<Rigidbody2D>();
+        // Gets a normalized direction for the ball's movement.
+        direction = Vector2.one.normalized;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
+        // Shoots the ball when the space bar is pressed and there isn't a ball in play.
+        if (Input.GetKeyDown(KeyCode.Space) && !playBall)
+        {
+            playBall = true;
+            ballRb.linearVelocity = direction * ballSpeed;
+        }
+        // Attaches the ball on top of the paddle if the ball is not in play.
         if (!playBall)
         {
             transform.position = paddleScript.paddlePos + (Vector2.up * ballOnPadOffset);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Paddle")
+        {
+            // Gets the paddle's half width.
+            float halfPaddle = collision.collider.bounds.size.x;
+            // Calculates how far the point of collision is from the paddle's half.
+            float x = (transform.position.x - collision.transform.position.x) / halfPaddle;
+            // Calculates the direction of the ball upon colliding with the paddle.
+            direction = new(newDirFactor * x, 1);
+            direction = direction.normalized;
+            // Mantains the ball's current velocity upon bouncing on the paddle.
+            float currentBallSpeed = ballRb.linearVelocity.magnitude;
+            ballRb.linearVelocity = direction * currentBallSpeed;
+        }
+        else if (collision.gameObject.tag == "Vertical Wall")
+        {
+            ballRb.linearVelocityY = - ballRb.linearVelocity.y;
         }
     }
 }
--- Assets/paddleScript.cs
+++ Assets/paddleScript.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    public float speed;
+    // Variable for controlling the paddle's speed.
+    public float paddleSpeed;
+    // Variable for storing the paddle's current psoition.
     public Vector2 paddlePos;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created.
     void Start()
     {
         gameObject.name = "Paddle";
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
         bool left = Input.GetKey(KeyCode.A);
         bool right = Input.GetKey(KeyCode.D);
+        // Paddle controls. If A is pressed on the keyborad, paddle will move to the left. If the paddle reaches the leftmost side of the screen, the paddle won't move any further.
         if (left && transform.position.x > -8)
         {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
+            transform.Translate(Vector2.left * Time.deltaTime * paddleSpeed);
         }
+        // Paddle controls. If D is pressed on the keyborad, paddle will move to the right. If the paddle reaches the righttmost side of the screen, the paddle won't move any further.
         else if (right && transform.position.x < 8)
         {
-            transform.Translate(Vector2.right * Time.deltaTime *speed);
+            transform.Translate(Vector2.right * Time.deltaTime *paddleSpeed);
         }
+        // Paddle's current position is stored in this public variable which is accesed by the Ball class.
         paddlePos = transform.position;
     }
 }
